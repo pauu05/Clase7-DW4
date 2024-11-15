962bd6a8dec0de4d3f4fcd6175d248279fe10abc
pauu05 <paulinahenry25@gmail.com>
2024-11-15 04:13:45 -0300

    Actualiza el archivo package.json

--- controllers/delete.js
+++ controllers/delete.js
@@ -1,12 +1,14 @@
-import {Post} from "../models/post.js";
+import { Post } from "../models/post.js";
 
-export const deletePost = async () => {
-    await Post.findByIdAndDelete(req.params.id, (err) => {
-        if (err) {
-            console.log("Error al eliminar el post", err);
-            res.status(500).send("Error interno del servidor");
-        } else {
-            res.send("Post eliminado exitosamente");
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send("Post no encontrado");
         }
-    })
-}
+        res.send("Post eliminado exitosamente");
+    } catch (err) {
+        console.error("Error al eliminar el post", err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
